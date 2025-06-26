1ab3fac70391e095c68855289e8310b23a389146
Dao Gia Bao <dgbao.cntt@gmail.com>
2025-06-26 18:02:10 +0700

    checj

--- obj/Debug/net9.0/JollibeeClone.AssemblyInfo.cs
+++ obj/Debug/net9.0/JollibeeClone.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JollibeeClone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+246b14bc5c005085e764f5c5d85d1b026097117a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7078df5b761de08563a306ecbc12dde4d52d856")]
 [assembly: System.Reflection.AssemblyProductAttribute("JollibeeClone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JollibeeClone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
